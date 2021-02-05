26db7424d3c47c5dddb709c05fd7de6a4c96c3d1
DBHeart <deity98@gmail.com>
2021-02-05 14:30:50 +0900

    기본 스프링부트 web 완성

---
+++ spring/security/README.md
@@ -0,0 +1,9 @@
+## spec
+1. maven
+2. spring-web
+3. spring-boot
+4. thymeleaf
+
+우선은 Spring Security 기본 메뉴얼을 따라해본다.
+
+
---
+++ spring/security/src/main/java/kr/pe/deity/security/SecurityApplication.java
@@ -0,0 +1,13 @@
+package kr.pe.deity.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApplication.class, args);
+    }
+
+}
